5beb1c4d39b39a2fc89c342239aaf067e3bd6934
Rick van Blitterswijk <vanblitterswijkrick@gmail.com>
2021-05-13 11:35:32 +0200

    Fetch stories data from API

--- src/App.js
+++ src/App.js
@@ -12,33 +12,7 @@ const useSemiPersistentState = (key, initialState) => {
   return [value, setValue];
 };
 
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const getAsyncStories = () => {
-  return new Promise (resolve => 
-    setTimeout( 
-      () => resolve({ data: { stories: initialStories } }),
-      2000
-    )
-  );
-}
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const storiesReducer = (state, action) => {
   switch(action.type) {
@@ -82,11 +56,12 @@ function App() {
   React.useEffect(() => {
     dispatchStories({type: 'STORIES_FETCH_INIT'});
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`)
+      .then(response => response.json())
       .then(result => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.stories
+          payload: result.hits,
         });
       })
       .catch(() => { 
