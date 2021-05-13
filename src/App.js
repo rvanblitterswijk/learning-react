a3b7baf11ff6af4c85638ab3d61831b584a70b18
Rick van Blitterswijk <vanblitterswijkrick@gmail.com>
2021-05-13 12:14:18 +0200

    Use axios for explicit GET requests instead of broswer native fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -45,7 +46,7 @@ const storiesReducer = (state, action) => {
   }
 }
 
-function App() {
+const App = () => {
   const [stories, dispatchStories] = React.useReducer(
     storiesReducer, 
     { data: [], isLoading: false, isError: false }
@@ -62,16 +63,13 @@ function App() {
   }
 
   const handleFetchStories = React.useCallback(() => {
-    if (!searchTerm) return;
-
     dispatchStories({type: 'STORIES_FETCH_INIT'});
 
-    fetch(url)
-      .then(response => response.json())
+    axios.get(url)
       .then(result => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits,
+          payload: result.data.hits,
         });
       })
       .catch(() => { 
